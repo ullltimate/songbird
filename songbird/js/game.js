d16adf4a65c0ef91ae1ba48c02be49bd1f556ded
Svitlana Ponkratenko <ullltimate.zp@gmail.com>
2022-11-12 12:15:39 +0300

    feat: add next level in quiz

--- songbird/js/game.js
+++ songbird/js/game.js
@@ -2,6 +2,8 @@ import birdsData from '../js/birds.js';
 console.log(birdsData);
 var warmUpArray = birdsData[0];
 console.log(warmUpArray);
+var birdsDataFlat = birdsData.flat();
+console.log(birdsDataFlat);
 
 var optionsList = document.querySelector('.options-list');
 var levelList = document.querySelectorAll('.level-link');
@@ -9,22 +11,27 @@ console.log(levelList);
 var questionAudio = document.querySelector('.question-bock>audio');
 var questionName;
 var questionImg;
+var questionTitle = document.querySelector('.question-title');
+var questionImage = document.querySelector('.question-img');
+var btnNextLevel = document.querySelector('.btn-level');
 
 for (var i=0; i<levelList.length; i++){
     if(levelList[i].classList.contains('active') === true){
-        randomQuestion();
+        randomQuestion(warmUpArray);
     }
 }
 
-function randomQuestion(){
+function randomQuestion(array){
     var randomId = random(1,6);
     console.log(randomId);
-    for (var i=0; i<warmUpArray.length; i++){
-        if(warmUpArray[i].id === randomId){
-            questionAudio.src = warmUpArray[i].audio;
+    questionTitle.innerHTML = '******';
+    questionImage.src = "../../assets/img/bird2.png";
+    for (var i=0; i<array.length; i++){
+        if(array[i].id === randomId){
+            questionAudio.src = array[i].audio;
             questionAudio.load();
-            questionName = warmUpArray[i].name;
-            questionImg = warmUpArray[i].image;
+            questionName = array[i].name;
+            questionImg = array[i].image;
         }
     }
 }
@@ -32,9 +39,7 @@ function randomQuestion(){
 console.log(questionAudio);
 console.log(questionName);
 
-var questionTitle = document.querySelector('.question-title');
-var questionImage = document.querySelector('.question-img');
-var btnNextLevel = document.querySelector('.btn-level');
+
 
 function check(clickName, questName, clickElem){
     if(clickName === questName){
@@ -51,7 +56,7 @@ function check(clickName, questName, clickElem){
 optionsList.addEventListener('click', (event) => {
     var target = event.target;
     console.log(target.innerHTML);
-    changeDidcriptions(target.innerHTML);
+    changeDidcriptions(target.innerHTML, birdsDataFlat);
     check(target.innerHTML, questionName, target);
 })
 
@@ -63,20 +68,64 @@ var cardSpecBird = document.querySelector('.item-spec');
 var cardAudioBird = document.querySelector('.item-audio>audio');
 var cardDescrirtion =document.querySelector('.card-description');
 
-function changeDidcriptions(name){
+function changeDidcriptions(name, array){
     instruction.style.display = 'none';
     descripCard.style.display = 'flex';
-    for (var i=0; i<warmUpArray.length; i++){
-        if(warmUpArray[i].name === name){
-            cardImgBird.src = warmUpArray[i].image;
-            cardNameBird.innerText = warmUpArray[i].name;
-            cardSpecBird.innerText = warmUpArray[i].species;
-            cardAudioBird.src = warmUpArray[i].audio;
+    for (var i=0; i<array.length; i++){
+        if(array[i].name === name){
+            cardImgBird.src = array[i].image;
+            cardNameBird.innerText = array[i].name;
+            cardSpecBird.innerText = array[i].species;
+            cardAudioBird.src = array[i].audio;
             cardAudioBird.load();
-            cardDescrirtion.innerText = warmUpArray[i].description;
+            cardDescrirtion.innerText = array[i].description;
         }
     }
 }
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var o=1;
+var j=1;
+btnNextLevel.addEventListener('click', () => {
+    activeLevel(levelList);
+    levelOptions(optionsItemArray, birdsData[j]);
+    randomQuestion(birdsData[j]);
+    j++
+})
+function activeLevel(arr){
+    for (var i=0; i<arr.length; i++){
+        if(arr[i].classList.contains('active')){
+            arr[i].classList.remove('active');
+            arr[o].classList.add('active');
+            console.log(o);
+        }
+    }
+    if(btnNextLevel.classList.contains('win')){
+        btnNextLevel.classList.remove('win');
+        btnNextLevel.disabled = true;
+    }
+    o++;
+}
+console.log(document.querySelector('.active'))
+function nextLevel(){
+
+}
+
+var optionsItemArray = document.querySelectorAll('.option-item');
+console.log(optionsItemArray)
+
+function levelOptions(optArray, array){
+    for (var i=0; i<optArray.length; i++){
+        optArray[i].innerHTML = array[i].name;
+        if (optArray[i].classList.contains('win') || optArray[i].classList.contains('lose')){
+            optArray[i].classList.remove('win');
+            optArray[i].classList.remove('lose');
+        }
+    }
+    if(instruction.style.display === 'none'){
+        instruction.style.display = 'block';
+        descripCard.style.display = 'none';
+    }
 }
