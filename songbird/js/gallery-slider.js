da0663d5a8a44363a97c3aa3bb1a032ece2a889c
Svitlana Ponkratenko <ullltimate.zp@gmail.com>
2022-11-22 12:29:04 +0300

    feat: add translate gallery-page

--- songbird/js/gallery-slider.js
+++ songbird/js/gallery-slider.js
@@ -1,7 +1,21 @@
 import birdsData from '../js/birds.js';
+import birdsDataEn from '../js/birdsEn.js';
+import i18ObjGal from '../js/translate-gallery-page.js';
+console.log(localStorage.getItem('lang'));
 
 var btnRight = document.querySelector('.main-arr-right');
 var btnLeft = document.querySelector('.main-arr-left');
+
+var en = i18ObjGal.en;
+function getTranslate(lang){
+    var arrData = document.querySelectorAll('[data-i18n]');
+    for (var i=0; i<arrData.length; i++){
+        arrData[i].innerText = Object.values(lang)[i];
+    }
+}
+if(localStorage.getItem('lang') === 'en'){
+    getTranslate(en);
+}
 
 var i=1;
 
@@ -9,8 +23,11 @@ function sliderNext(){
     if(i>35){
         i=0;
     }
-    var birdDataNew = birdsData.flat();
-
+    if(localStorage.getItem('lang') === 'en'){
+        var birdDataNew = birdsDataEn.flat();
+    } else {
+        var birdDataNew = birdsData.flat();
+    }
     var nameBird = document.querySelector('.main-card-name');
     var imgBird = document.querySelector('.main-card-img');
     var speciesBird = document.querySelector('.main-cadr-species');
@@ -41,9 +58,11 @@ function sliderPrev(){
     if(i<0){
         i=35;
     }
- 
-    var birdDataNew = birdsData.flat();
-
+    if(localStorage.getItem('lang') === 'en'){
+        var birdDataNew = birdsDataEn.flat();
+    } else {
+        var birdDataNew = birdsData.flat();
+    }
     var nameBird = document.querySelector('.main-card-name');
     var imgBird = document.querySelector('.main-card-img');
     var speciesBird = document.querySelector('.main-cadr-species');
---
+++ songbird/js/translate-gallery-page.js
@@ -0,0 +1,19 @@
+const i18ObjGal = {
+    'en': {
+        'header-link-1': 'Home',
+        'header-link-2': 'Quiz',
+        'header-link-3': 'Gallery',
+        'main-title-text-1': 'Quiz Gallery',
+        'main-card-name-1': 'Raven',
+        'main-card-descr-1': 'Raven is a large bird. The body length reaches 70 centimeters, the wingspan is up to one and a half meters. Ravens inhabit the vicinity of the Tower. There is a belief in England that the day the black crows fly away from the Tower, the monarchy will collapse.'
+    },
+    'ru': {
+        'header-link-1': 'Об игре',
+        'header-link-2': 'Викторина',
+        'header-link-3': 'Галерея',
+        'main-title-text-1': 'Галерея викторины',
+        'main-card-name-1': 'Ворон',
+        'main-card-descr-1': 'Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.'
+    }
+}
+export default i18ObjGal;
